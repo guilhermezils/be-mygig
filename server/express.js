20b96491b1fc987a76c26890c238663a45e07aff
guilhermezils <guilhermezils@icloud.com>
2021-08-05 16:24:57 -0500

    usermodel.step1

--- server/express.js
+++ server/express.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import bodyParser from 'body-parser'
+// import bodyParser from 'body-parser'// no need to use it anymore, depracated
 import cookieParser from 'cookie-parser'
 import compress from 'compression'
 import cors from 'cors'
 import helmet from 'helmet'
+import Template from './../template'
 
 const app = express()
 
@@ -13,5 +14,8 @@ app.use(cookieParser())
 app.use(compress())
 app.use(helmet())
 app.use(cors())
+app.get('/', (req, res) => {
+  res.status(200).send(Template())
+ })
 
 export default app
