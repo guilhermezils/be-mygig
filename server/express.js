e58b74210d412dba19e24118eff1befc65a3eb11
guilhermezils <guilhermezils@icloud.com>
2021-08-05 15:54:17 -0500

    express configured

--- server/express.js
+++ server/express.js
@@ -1,4 +1,17 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compress from 'compression'
+import cors from 'cors'
+import helmet from 'helmet'
+
 const app = express()
-  /*... configure express ... */
+
+app.use(express.json()) //no need to use bodyParser anymore
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(compress())
+app.use(helmet())
+app.use(cors())
+
 export default app
